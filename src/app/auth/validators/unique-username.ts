96f8d663dcff28fb640f6feb2f447bfa19ac5624
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2021-03-07 16:02:52 -0300

    Validação

---
+++ src/app/auth/validators/unique-username.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator } from '@angular/forms';
+import { of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { AuthService } from '../auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UniqueUsername implements AsyncValidator {
+  constructor(private authService: AuthService) {}
+
+  validate = (control: AbstractControl) => {
+    const { value } = control;
+    return this.authService.usernameAvailable(value).pipe(
+      map((res) => {
+        if (res.available) {
+          return null;
+        }
+      }),
+      catchError((err) => {
+        if (err.error.username) {
+          return of({ nonUniqueUsername: true });
+        } else {
+          return of({ noConnection: true });
+        }
+      })
+    );
+  };
+}
